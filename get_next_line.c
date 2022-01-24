6afd30097789ccd3d0830f074acf9ed2e3e7f54a
David Wang <devtesting404@gmail.com>
2022-01-24 18:36:18 +0800

    Finished version

--- get_next_line.c
+++ get_next_line.c
@@ -1,20 +1,4 @@
 #include "get_next_line.h"
-#include <string.h>
-
-char	*ft_strdup(const char *string)
-{
-	char	*dup;
-	int		i;
-
-	dup = malloc(sizeof(char) * (strlen(string) + 1));
-	if (!dup)
-		return (NULL);
-	i = -1;
-	while (string[++i])
-		dup[i] = string[i];
-	dup[i] = '\0';
-	return (dup);
-}
 
 char	*get_next_line(int fd)
 {
@@ -58,9 +42,7 @@ char	*get_next_line(int fd)
 	line = ft_strndup(read_str, end_ptr - read_str + !!ft_strchr(read_str, '\n'));
 	if (ft_strchr(read_str, '\n'))
 	{
-		temp = ft_strdup(end_ptr + 1);
-		// temp = ft_strndup(end_ptr + 1, ft_strlen(read_str));
-		// temp = ft_substr(read_str, end_ptr - read_str + 1, ft_strlen(read_str));
+		temp = ft_strndup(end_ptr + 1, ft_strlen(end_ptr + 1));
 		free(read_str);
 		read_str = temp;
 	}
